8b0828c15f09ac9e96b48eed99db70c103995234
nguyenvoankhoa <nguyenvoankhoa@gmail.com>
2023-08-03 00:10:29 +0700

    update date admin

--- src/pages/Admin/AdminHome.js
+++ src/pages/Admin/AdminHome.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import CategoryAmount from "../../components/Admin/CategoryAmount";
-import { PieChart, Pie, Sector, ResponsiveContainer, Cell } from 'recharts';
+import { PieChart, Pie, Sector, ResponsiveContainer, Cell } from "recharts";
 import { DatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
-const COLORS = ['#EC6940', '#2B82B4', '#A2D59F', '#F4A761', '#F4A761'];
+const COLORS = ["#EC6940", "#2B82B4", "#A2D59F", "#F4A761", "#F4A761"];
 
 const renderActiveShape = (props) => {
   const RADIAN = Math.PI / 180;
-  const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, amount } = props;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    amount,
+  } = props;
   const sin = Math.sin(-RADIAN * midAngle);
   const cos = Math.cos(-RADIAN * midAngle);
   const sx = cx + (outerRadius + 10) * cos;
@@ -19,11 +31,18 @@ const renderActiveShape = (props) => {
   const my = cy + (outerRadius + 30) * sin;
   const ex = mx + (cos >= 0 ? 1 : -1) * 22;
   const ey = my;
-  const textAnchor = cos >= 0 ? 'start' : 'end';
+  const textAnchor = cos >= 0 ? "start" : "end";
 
   return (
     <g>
-      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill} style={{ fontSize: "20px" }}>
+      <text
+        x={cx}
+        y={cy}
+        dy={8}
+        textAnchor="middle"
+        fill={fill}
+        style={{ fontSize: "20px" }}
+      >
         {payload.category}
       </text>
       <Sector
@@ -44,10 +63,25 @@ const renderActiveShape = (props) => {
         outerRadius={outerRadius + 10}
         fill={fill}
       />
-      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
       <circle cx={ex} cy={ey} r={4} fill={fill} stroke="none" />
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`Số lượng ${amount}`}</text>
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#333"
+      >{`Số lượng ${amount}`}</text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999"
+      >
         {`(Tỉ lệ ${(percent * 100).toFixed(2)}%)`}
       </text>
     </g>
@@ -59,8 +93,7 @@ const AdminHome = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [dayStart, setDayStart] = useState();
   const [dayEnd, setDayEnd] = useState();
-  let processDate = {};
-  console.log("start: ", dayStart, "end: ", dayEnd);
+  const [processDate, setProcessDate] = useState();
 
   const onPieEnter = (event, index) => {
     setActiveIndex(index);
@@ -68,19 +101,19 @@ const AdminHome = () => {
 
   useEffect(() => {
     if (dayStart !== undefined && dayEnd !== undefined) {
-      const formattedStartDate = dayStart.toISOString().split("T")[0];
-      const formattedEndDate = dayEnd.toISOString().split("T")[0];
-      processDate = {
+      let formattedStartDate = dayStart.toISOString().split("T")[0];
+      let formattedEndDate = dayEnd.toISOString().split("T")[0];
+      let dateReq = {
         dateStart: formattedStartDate,
-        dateEnd: formattedEndDate
-      }
+        dateEnd: formattedEndDate,
+      };
+      setProcessDate(dateReq);
     }
   }, [dayStart, dayEnd]);
 
   useEffect(() => {
     async function processData() {
       const token = sessionStorage.getItem("jwtToken");
-      console.log("Token: ", token)
       const apiUrl = process.env.REACT_APP_API_URL;
       const res = await fetch(apiUrl + "admin/amount", {
         method: "POST",
@@ -101,13 +134,17 @@ const AdminHome = () => {
     if (dayStart !== undefined && dayEnd !== undefined) {
       processData();
     }
-  }, [processDate])
+  }, [processDate]);
 
-  return <>
-    {/* <CategoryAmount /> */}
-    <div className="container d-flex mt-5" style={{ height: "60vh", justifyContent: "space-between" }}>
-      {/* <ResponsiveContainer width="45%" height="100%"> */}
-      {/* <PieChart width={400} height={400}>
+  return (
+    <>
+      {/* <CategoryAmount /> */}
+      <div
+        className="container d-flex mt-5"
+        style={{ height: "60vh", justifyContent: "space-between" }}
+      >
+        {/* <ResponsiveContainer width="45%" height="100%"> */}
+        {/* <PieChart width={400} height={400}>
           <Pie
             activeIndex={activeIndex}
             activeShape={renderActiveShape}
@@ -124,31 +161,28 @@ const AdminHome = () => {
             ))}
           </Pie>
         </PieChart> */}
-      {/* </ResponsiveContainer> */}
-      <div style={{ width: "40%" }}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            disablePast
-            label="Chọn ngày bắt đầu"
-            format="DD/MM/YYYY"
-            // value={dayStart}
-            onChange={(date) => setDayStart(date)}
-          />
-        </LocalizationProvider>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            disablePast
-            label="Chọn ngày kết thúc"
-            format="DD/MM/YYYY"
-            // value={dayEnd}
-            onChange={(date) => setDayEnd(date)}
-          />
-        </LocalizationProvider>
-
+        {/* </ResponsiveContainer> */}
+        <div style={{ width: "40%" }}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Chọn ngày bắt đầu"
+              format="DD/MM/YYYY"
+              // value={dayStart}
+              onChange={(date) => setDayStart(date)}
+            />
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Chọn ngày kết thúc"
+              format="DD/MM/YYYY"
+              // value={dayEnd}
+              onChange={(date) => setDayEnd(date)}
+            />
+          </LocalizationProvider>
+        </div>
       </div>
-    </div>
-  </>;
+    </>
+  );
 };
 
 export default AdminHome;
-
